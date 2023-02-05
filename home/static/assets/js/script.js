0723fd480db986a23633cde2d6678404211911a7
Mueem-Nahid <mueem51@gmail.com>
2023-02-05 20:03:00 +0600

    # Feature - password toggle functionality

--- home/static/assets/js/script.js
+++ home/static/assets/js/script.js
@@ -63,3 +63,17 @@ function generatePasswordHandler() {
         }
     });
 };
+
+function toggleView(id) {
+    const input = document.getElementById(id);
+    const icon = document.getElementById(`icon-${id}`)
+    if (input.type === "password") {
+        input.type = "text";
+        icon.classList.remove("fa-eye")
+        icon.classList.add("fa-eye-slash")
+    } else {
+        input.type = "password";
+        icon.classList.remove("fa-eye-slash")
+        icon.classList.add("fa-eye")
+    }
+}
