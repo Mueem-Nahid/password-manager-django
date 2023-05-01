5627b14d0685be4d97e0052c69de1f70cddb8568
Mueem-Nahid <mueem51@gmail.com>
2023-05-01 16:19:25 +0600

    # Info - build.sh added - dependency for deployment

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = Config.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = Config.DEBUG_MODE
 
 ALLOWED_HOSTS = []
 
