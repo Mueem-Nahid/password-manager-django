db039b022e80cbd9c2f7e1937eefc242845299fd
Mueem-Nahid <mueem51@gmail.com>
2023-05-01 17:18:31 +0600

    # Info - STATIC_ROOT added

--- core/settings.py
+++ core/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'home/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
